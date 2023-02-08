422435a1cc44080eba019c54c5398f2e0641d806
maxOSfushta <fushta.m1997333@gmail.com>
2023-02-08 18:17:34 +0200

    minor bug fixed

--- thumbnails.js
+++
@@ -1,6 +0,0 @@
-var imageThumbs = document.getElementById("image-thumbs");
-for (var i = 1; i <= 6; i++) {
-    var thumb = document.createElement("img");
-    thumb.src = "gallery/image" + i + ".webp";
-    thumb.alt = "Image " + i;
-}
